16752ddfdd1da317a254c88b292460e10b8dd878
hjkim <hhaa407@naver.com>
2020-05-27 22:03:54 +0900

    add bg image, weather

--- todo.js
+++ todo.js
@@ -31,7 +31,7 @@ function paintToDo(text) {
     const delBtn = document.createElement("button");
     const span = document.createElement("span");
     const newId = toDos.length + 1;
-    delBtn.innerText = "X";
+    delBtn.innerText = "❌";
     delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
     li.appendChild(span);
